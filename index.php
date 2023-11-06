8a5920032492d22b007af190389b9b9f7d9b28c5
rph2002 <rpharrison02@gmail.com>
2023-11-06 16:52:45 -0600

    Added login. Fixed minor errors

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>Hello World!<h1>
+<?php
+include 'functions.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$dblink=db_connect();
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+$sql = "SELECT * FROM user WHERE Email = '$email' AND Password = '$password'";
+$result = $dblink->query($sql);
+
+if ($result->num_rows > 0) {
+    // Email and password exist together
+    echo "Successfully logged in.";
+    header("Location: /home.html");
+} else {
+    // Email or password do not match
+    echo "Email or password do not match.";
+}
+
+$dblink->close();
+exit();
+?>
