e842916e89b825900ceacef7877d010a68bc09d8
rph2002 <rpharrison02@gmail.com>
2023-11-06 16:41:45 -0600

    Fixed registration page to redirect to registration.php

--- registration.php
+++ registration.php
@@ -1,19 +1,16 @@
 <?php
+include 'functions.php';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'functions.php';
 $dblink=db_connect();
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
 
 // Check if email already exists in table
 $email = $_POST["email"];
 $sql = "SELECT * FROM user WHERE Email = '$email'";
-$result = $conn->query($sql);
+$result = $dblink->query($sql);
 
 if ($result->num_rows > 0) {
     // Email already exists, display error message
@@ -25,14 +22,14 @@ if ($result->num_rows > 0) {
     $password = $_POST["password"];
     $sql = "INSERT INTO user (First_Name, Last_Name, Email, Password, Phone_Number) VALUES ('$first_name', '$last_name', '$email', '$password', '$phone_number')";
 
-    if ($conn->query($sql) === TRUE) {
+    if ($dblink->query($sql) === TRUE) {
         echo "New user created successfully.";
         header("Location: /home.html");
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $sql . "<br>" . $dblink->error;
     }
 }
 
-$conn->close();
+$dblink->close();
 exit();
 ?>
