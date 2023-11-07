7a072915d5cfe6192e3be56ac648f6defcaf1732
rph2002 <rpharrison02@gmail.com>
2023-11-07 17:24:50 -0600

    Fixed syntax error

--- nav_bar.php
+++ nav_bar.php
@@ -8,13 +8,13 @@ function generate_nav_bar() {
     echo "<nav>";
     echo "<ul>";
     foreach ($pages as $page => $url) {
-        if ($page == $_SESSION("active_page")) {
+        if ($page == $_SESSION["active_page"]) {
             echo "<li><a href=\"$url\" class=\"active\">$page</a></li>";
         } else {
             echo "<li><a href=\"$url\">$page</a></li>";
         }
     }
-    if ($_SESSION("role") == "Admin") {
+    if ($_SESSION["role"] == "Admin") {
         echo "<li><a href=\"admin.php\">Admin</a></li>";
     }
     echo "</ul>";
