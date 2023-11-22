295ef80478595fcd2466964c2d8523a0527aabd4
rph2002 <rpharrison02@gmail.com>
2023-11-22 00:25:25 -0600

    Updated nav-bar

--- index.php
+++ index.php
@@ -57,12 +57,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="row mt-5">
       <div class="col-lg-4 bg-white m-auto">
 
-        <form id="login-form" action="index.php" method="post" autocomplete="on">
+        <form id="row g-3 needs-validation" action="index.php" method="post" autocomplete="on">
           <h1 class="text-center pt-3 mb-3">Login</h1>
 
           <div class="form-floating mb-3">
             <input type="email" class="form-control" id="email" name="email" placeholder="Email" required />
             <label for="email">Email: </label>
+            <div class="invalid-feedback">
+              Please provide a valid email.
+            </div>
           </div>
 
           <div class="form-floating mb-3">
@@ -93,6 +96,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+  <script>
+    /* Bootstrap validation (not working)*/
+    /* (() => {
+      'use strict'
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      const forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })() */
+  </script>
 </body>
 
 </html>
