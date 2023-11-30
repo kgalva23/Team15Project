7bb6c42d71424692d2a06191fa0334214aa7d630
Kaleb Galvan <99695431+kgalva23@users.noreply.github.com>
2023-11-29 22:34:38 -0600

    Update home.php
    Fixed list of 'Popular Items' to display item images.

--- home.php
+++ home.php
@@ -92,21 +92,19 @@ $dblink->close();
                 <?php foreach ($items as $item): ?>
                     <div class="col-md-6 mb-4">
                         <div class="card card-flex">
-                            <img src="<?php echo htmlspecialchars($item['ImagePath']); ?>" alt="<?php echo 'Image not found for: ' . htmlspecialchars($item['Name']); ?>">
+                            <img src="<?php echo $_SESSION['s3url'] . htmlspecialchars($item['ImagePath']); ?>">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo htmlspecialchars($item['Name']); ?></h5>
                                 <p class="card-text"><?php echo htmlspecialchars($item['Description']); ?></p>
                                 <p class="card-text">Company: <?php echo htmlspecialchars($item['Company']); ?></p>
                                 <p class="card-text">Price: $<?php echo htmlspecialchars(number_format($item['Price'], 2)); ?></p>
                                 <p class="card-text">Available: <?php echo htmlspecialchars($item['Stock']); ?></p>
-                                <button class="btn btn-success">Add to Cart</button>
+                                <button class="btn btn-success add-to-cart" data-item-id="<?php echo $item['Item_ID']; ?>">Add to Cart</button>
                             </div>
                         </div>
                     </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-        </div>
+                <?php endforeach; ?>     
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
