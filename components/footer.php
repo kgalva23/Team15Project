0e2e1fba1bae1c872b73119144a2e0671cfbd9ee
rph2002 <rpharrison02@gmail.com>
2023-11-29 23:42:33 -0600

    Added footer function and stylesheet

---
+++ components/footer.php
@@ -0,0 +1,26 @@
+<?php
+
+function generate_footer()
+{
+    echo '
+            <footer class="bg-dark text-light">
+                <div class="container py-0.1">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h3>Contact Us</h3>
+                            <p>123 Grocery Street, Apple City, USA</p>
+                            <p>Email: info@freshpicks.com</p>
+                            <!--    <p>Phone: 123-456-7890</p>  -->
+                </div>
+                <div class="col-md-4">
+                    <h3>Share With Others</h3>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="https://www.facebook.com/login" target="_blank">Facebook</a></li>
+                        <li class="list-inline-item"><a href="https://twitter.com/login" target="_blank">Twitter/X</a></li>
+                        <li class="list-inline-item"><a href="https://www.instagram.com/accounts/login/" target="_blank">Instagram</a></li>
+                    </ul>
+                </div>
+                </div>
+                </div>
+            </footer>';
+}
